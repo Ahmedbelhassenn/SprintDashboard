4dc9d953f2b601685467c155bfa13cea6696f805
AhmedBelhassenn <ahmed.belhassen@supcom.tn>
2025-07-30 16:34:16 +0100

    Exclude FeignAutoConfiguration in test to fix ApplicationContext loading

--- SprintDash_Backend/src/test/java/com/example/sprintdash/SprintDashApplicationTests.java
+++ SprintDash_Backend/src/test/java/com/example/sprintdash/SprintDashApplicationTests.java
@@ -1,11 +1,12 @@
 package com.example.sprintdash;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
-
+@EnableAutoConfiguration(exclude = {FeignAutoConfiguration.class})
 @SpringBootTest
 class SprintDashApplicationTests {
 
