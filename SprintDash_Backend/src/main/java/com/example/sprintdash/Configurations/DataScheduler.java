7d13315c498a6f71870e30c14a7b74f90bcc1508
AhmedBelhassenn <ahmed.belhassen@supcom.tn>
2025-08-04 10:53:12 +0100

    Modifier dataScheduler

--- SprintDash_Backend/src/main/java/com/example/sprintdash/Configurations/DataScheduler.java
+++ SprintDash_Backend/src/main/java/com/example/sprintdash/Configurations/DataScheduler.java
@@ -2,10 +2,12 @@ package com.example.sprintdash.Configurations;
 
 import com.example.sprintdash.Services.UpdateService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
+@ConditionalOnProperty(name = "scheduling.enabled", havingValue = "true", matchIfMissing = true)
 public class DataScheduler {
     @Autowired
     UpdateService updateService;
